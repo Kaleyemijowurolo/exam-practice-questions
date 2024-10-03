789673dfeff02e45739cb0262692304a28232bd2
chimae <chimandukwe.r@gmail.com>
2024-10-03 16:12:58 +0000

    correct the error during deployment

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import React from "react";
 import profile from "../public/alex.jpg";
 
@@ -7,13 +6,6 @@ const Navbar: React.FC = () => {
     <div className="bg-blue-700 py-2 px-4 ">
       <div className="flex justify-between items-center">
         <p className="text-white font-bold text-xl"> EXAM PRACTICE</p>
-        {/* <Image
-          src={profile}
-          alt="profile"
-          width={50}
-          height={50}
-          className="rounded-full"
-        /> */}
       </div>
     </div>
   );
