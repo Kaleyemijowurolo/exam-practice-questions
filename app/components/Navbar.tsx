5491be2e694210a681e36b1cd87cc0f8a1346136
chimae <chimandukwe.r@gmail.com>
2024-10-04 07:41:57 +0000

    Resolved image issue

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import profile from "../public/alex.jpg";
 
@@ -5,7 +6,14 @@ const Navbar: React.FC = () => {
   return (
     <div className="bg-blue-700 py-2 px-4 ">
       <div className="flex justify-between items-center">
-        <p className="text-white font-bold text-xl"> EXAM PRACTICE</p>
+        <p className="text-white font-bold text-xl"> EXAM</p>
+        <Image
+          src={profile}
+          alt="profile"
+          width={30}
+          height={30}
+          className="rounded-full"
+        />
       </div>
     </div>
   );
