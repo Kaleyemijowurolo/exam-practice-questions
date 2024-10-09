4c1aab79800c52a76b59a85062e7cd01f46697c7
chimae <chimandukwe.r@gmail.com>
2024-10-09 13:39:55 +0000

    resolved issue with question defined but not used

--- app/assessment/page.tsx
+++ app/assessment/page.tsx
@@ -1,127 +1,3 @@
-// "use client";
-
-// import React, { useState, useEffect } from "react";
-// import { Container, Button, ProgressBar, Spinner } from "react-bootstrap";
-// import QuestionCard from "../components/QuestionCard";
-// import { useRouter } from "next/navigation";
-// import { Question } from "../types/types";
-// import axios from "axios";
-
-// const Assessment: React.FC = () => {
-//   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-//   const [userAnswers, setUserAnswers] = useState<string[]>([]);
-//   const [questionsData, setQuestionsData] = useState<Question[]>([]);
-//   const [loading, setLoading] = useState(true);
-//   const router = useRouter();
-//   const [timeRemaining, setTimeRemaining] = useState(60 * 10);
-//   const [timerRunning, setTimerRunning] = useState(false);
-
-//   useEffect(() => {
-//     // Fetch questions from the API
-//     const fetchQuestions = async () => {
-//       console.log("fetching...");
-//       try {
-//         const response = await axios.get("/api/questions");
-//         console.log(response.data, "response");
-//         const questions: Question[] = await response.data;
-
-//         setQuestionsData(questions);
-//         console.log(questions, "questions");
-//         setUserAnswers(Array(questions.length).fill(""));
-//         setLoading(false);
-//       } catch (error) {
-//         console.error("Failed to fetch questions:", error);
-//         setLoading(false);
-//       }
-//     };
-//     fetchQuestions();
-//   }, []);
-
-//   useEffect(() => {
-//     if (timerRunning && timeRemaining > 0) {
-//       const timer = setTimeout(() => {
-//         setTimeRemaining((prevTime) => prevTime - 1);
-//       }, 1000);
-//       return () => clearTimeout(timer);
-//     } else if (timeRemaining <= 0) {
-//       setTimerRunning(false);
-//       router.push("/assessment/result");
-//     }
-//   }, [timeRemaining, timerRunning, router]);
-
-//   const setAnswer = (index: number, answer: string) => {
-//     const updatedAnswers = [...userAnswers];
-//     updatedAnswers[index] = answer;
-//     setUserAnswers(updatedAnswers);
-//   };
-
-//   const handleNext = () => {
-//     if (currentQuestionIndex < questionsData.length - 1) {
-//       setCurrentQuestionIndex(currentQuestionIndex + 1);
-//     } else {
-//       sessionStorage.setItem("userAnswers", JSON.stringify(userAnswers));
-//       sessionStorage.setItem(
-//         "correctAnswers",
-//         JSON.stringify(questionsData.map((q) => q.correctAnswer))
-//       );
-
-//       // Use AssessmentResult when passing to the result page
-//       router.push("/result");
-//     }
-//   };
-
-//   if (loading) {
-//     return (
-//       <Container className="mt-4 text-center">
-//         <Spinner animation="border" role="status">
-//           <span className="visually-hidden">Loading...</span>
-//         </Spinner>
-//       </Container>
-//     );
-//   }
-
-//   return (
-//     <Container className="mt-4 w-screen h-[90vh] bg-blue-50">
-//       <div className="flex justify-between items-center px-4 py-4">
-//         <h2 className="text-2xl">Exam Assessment</h2>
-
-//         <div className="w-28 bg-blue-700 flex items-center justify-center px-1">
-//           <p className="text-xl font-bold text-white text-center">
-//             {`Timer: ${timeRemaining}s`}
-//           </p>
-//         </div>
-//       </div>
-//       <ProgressBar
-//         className=""
-//         now={((currentQuestionIndex + 1) / questionsData.length) * 100}
-//         label={`${currentQuestionIndex + 1}/${questionsData.length}`}
-//       />
-
-//       {questionsData.length > 0 && (
-//         <QuestionCard
-//           question={questionsData[currentQuestionIndex]}
-//           index={currentQuestionIndex}
-//           selectedAnswer={userAnswers[currentQuestionIndex]}
-//           setAnswer={setAnswer}
-//         />
-//       )}
-
-//       <div className="text-center">
-//         <Button
-//           variant="primary"
-//           onClick={handleNext}
-//           disabled={!userAnswers[currentQuestionIndex]}
-//         >
-//           {currentQuestionIndex === questionsData.length - 1
-//             ? "Submit"
-//             : "Next"}
-//         </Button>
-//       </div>
-//     </Container>
-//   );
-// };
-
-// export default Assessment;
 "use client";
 import { useEffect, useState } from "react";
 import { questions } from "../api/questionData"; // Your question data
@@ -130,13 +6,6 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { Container, Button, ProgressBar, Spinner } from "react-bootstrap";
 
-// Define types for question
-type Question = {
-  text: string;
-  options: string[];
-  correctAnswer: string;
-};
-
 const AllQuestions: React.FC = () => {
   const [timeRemaining, setTimeRemaining] = useState(60 * 10);
   const [timerRunning, setTimerRunning] = useState(true);
@@ -144,10 +13,9 @@ const AllQuestions: React.FC = () => {
   const router = useRouter();
 
   useEffect(() => {
-    // Simulate a loading delay
     setTimeout(() => {
       setLoading(false);
-    }, 2000); // Assume a 2 second load time for this example
+    }, 2000);
 
     if (timerRunning && timeRemaining > 0) {
       const timer = setTimeout(() => {
@@ -207,7 +75,7 @@ const AllQuestions: React.FC = () => {
     return (
       <Container className="mt-4 text-center">
         <Spinner animation="border" role="status">
-          <span className="visually-hidden">Loading...</span>
+          <span className="visually-hidden">{loading}</span>
         </Spinner>
       </Container>
     );
